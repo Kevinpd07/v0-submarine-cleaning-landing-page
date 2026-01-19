ccb42d387fd465063274ecb77f7da871c588358e
kevinpd07 <kevinpd1207@gmail.com>
2026-01-19 13:35:53 -0500

    add hero-mobile

--- components/hero.tsx
+++ components/hero.tsx
@@ -82,7 +82,7 @@ export function Hero() {
           className="hidden md:block object-cover"
         />
         <Image
-          src="/hero-mobile3.png"
+          src="/hero-mobile3.jpeg"
           alt="Underwater background"
           fill
           priority
